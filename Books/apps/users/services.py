6a648e1a4e076a8387f2471bf86f9324b3eb9a7b
Maksim Logvinov <m.logvinov@wis.software>
2023-09-04 14:43:57 +0300

    refactoring code

--- Books/apps/users/services.py
+++ Books/apps/users/services.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.models import Site
@@ -12,13 +13,16 @@ from django.utils.translation import gettext
 def send_message(user, url_name, subject, message):
     token = default_token_generator.make_token(user)
     uid = urlsafe_base64_encode(force_bytes(user.pk))
-    activation_url = reverse_lazy(url_name, kwargs={'uidb64': uid,
-                                                    'token': token})
+    activation_url = reverse_lazy(
+        url_name,
+        kwargs={'uidb64': uid, 'token': token}
+    )
     current_site = Site.objects.get_current().domain
     if current_site == 'example.com':
-        current_site = '127.0.0.1:8000'
+        current_site = os.environ.get('LOCALE_URL')
     send_mail(subject=subject,
               message=message + f'http://{current_site}{activation_url}',
-              recipient_list=[user.email], from_email='vvglvv1@yandex.ru',
+              recipient_list=[user.email],
+              from_email=os.environ.get('EMAIL_HOST_USER'),
               fail_silently=False)
     logging.log(51, gettext("Сообщение отправлено"))
